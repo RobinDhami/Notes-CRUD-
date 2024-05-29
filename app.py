f5edc9ad4edeca1a7327c593e3d635b3804aae27
RobinDhami <rozendhami69@gmail.com>
2024-05-29 20:16:42 +0545

    Add Task

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -19,10 +19,23 @@ class Todo(db.Model):
 # Initialize the database
 with app.app_context():
     db.create_all()
-
-@app.route('/')
+    
+@app.route('/',methods=['POST','GET'])
 def index():
-    return render_template("index.html")
+    if request.method=='POST':
+        task_content = request.form['content']
+        new_task = Todo(content = task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        
+        except:
+            return "There is an issue with your task"
+    else:
+        tasks=Todo.query.order_by(Todo.date_created).all()
+    return render_template("index.html",tasks=tasks)
 
 if __name__ == "__main__":
     app.run(debug=True)
